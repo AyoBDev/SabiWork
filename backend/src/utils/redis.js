a5b0bc0b276b894cc566d243377d06c991482b3d
AyoBDev <ayobamitunmise99@gmail.com>
2026-05-09 16:04:35 +0100

    feat: add Redis client utility

---
+++ backend/src/utils/redis.js
@@ -0,0 +1,21 @@
+// backend/src/utils/redis.js
+const Redis = require('ioredis');
+const config = require('../config');
+
+const redis = new Redis(config.redisUrl, {
+  maxRetriesPerRequest: 3,
+  retryStrategy(times) {
+    const delay = Math.min(times * 50, 2000);
+    return delay;
+  }
+});
+
+redis.on('error', (err) => {
+  console.error('Redis connection error:', err.message);
+});
+
+redis.on('connect', () => {
+  console.log('Redis connected');
+});
+
+module.exports = redis;
